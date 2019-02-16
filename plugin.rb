2a1453f423827966275876c2d489ef5356314d40
Kampfkarren <boynedmaster@gmail.com>
2019-02-15 23:17:06 -0800

    Closes #1 and #2

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,20 @@
 # name: dev-rel-automated-messages
-# version: 1.0.0
+# version: 1.1.0
 # authors: boyned/Kampfkarren
 
+enabled_site_setting :dev_rel_automated_messages
+
 after_initialize do
-	Discourse.module_eval do
-		def self.site_contact_user
-			Group.find_by(name: "DevRelationsTeam").users.sample
+	DiscourseEvent.on(:topic_created) do |topic|
+		if topic.subtype == TopicSubtype.system_message
+			if topic.title == I18n.t("system_messages.welcome_tl1_user.subject_template") ||
+				topic.title == I18n.t("system_messages.welcome_tl2_user.subject_template")
+			then
+				dev_rel = Group.find_by(name: "DevRelationsTeam").users.sample
+				topic.posts.first.set_owner(dev_rel, dev_rel, true)
+				topic.save
+				topic.reload
+			end
 		end
 	end
 end
--- spec/integration/dev_rel_automated_messages_spec.rb
+++ spec/integration/dev_rel_automated_messages_spec.rb
@@ -2,21 +2,36 @@ require "rails_helper"
 
 RSpec.describe "Dev Rel Automated Messages" do
 	let(:dev_rel) { Fabricate(:group, name: "DevRelationsTeam") }
+	let(:user) { Fabricate(:user) }
 
 	before do
 		5.times do |i|
 			dev_rel.add(Fabricate(:user, username: "DevRel" + i.to_s))
 		end
+
+		SiteSetting.site_contact_username = "system"
 	end
 
-	it "should send messages from dev rel instead of system" do
+	it "shouldn't override site_contact_username" do
+		expect(SiteSetting.site_contact_username).to eq("system")
+	end
+
+	it "should send promo messages from dev rel instead of system" do
 		captured = [false, false, false, false, false]
 		5.times do
-			user = Discourse.site_contact_user
+			system_message = SystemMessage.new(user).create("welcome_tl1_user")
+			system_message.reload
+			user = system_message.user
 			expect(dev_rel.user_ids).to include(user.id)
 			dev_rel.remove user
 			captured[user.username[-1].to_i] = true
 		end
 		expect(captured).not_to include(false)
+	end
+
+	it "should send other system messages through system" do
+		system_message = SystemMessage.new(user).create("random_system_mesage")
+		system_message.reload
+		expect(system_message.user.username).to eq("system")
 	end
 end
